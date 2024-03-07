b84b99eac32dc8e314e81dd0bd4e6beccb773b95
cyril <cyrilschool12@gmail.com>
2024-03-07 18:39:46 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header flex flex-col gap-3">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md">
+          git demonstration
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
