f820db4d4222c0d99510056b1859526bce08c492
Cyril Jan Alonzo <cyril.alonzo@lastfarewells.com>
2024-03-14 19:34:49 +0800

    Implement viewing of to-do

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header flex flex-col gap-3">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1 className="text-3xl font-bold">Hello world!</h1>
-        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md">
-          git demonstration
+        <h1 className="text-3xl font-bold">To do list App!</h1>
+        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md w-full">
+          My to do 1
+        </p>
+        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md w-full">
+          My to do 2
+        </p>
+        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md w-full">
+          My to do 3
+        </p>
+        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md w-full">
+          My to do 4
+        </p>
+        <p className=" text-slate-300 bg-indigo-500 p-3 rounded-md w-full">
+          My to do 5
         </p>
       </header>
     </div>
